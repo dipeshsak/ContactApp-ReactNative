1fc7047ffa18edace58c35b6569b920b0a75495e
Dipesh <dipesh.sakharkar@otiselevator.com>
2020-05-31 23:17:24 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from "react-navigation-stack";
+
+import HomeScreen from './screens/HomeScreen';
+import AddNewContactScreen from './screens/AddNewContactScreen'
+import EditContactScreen from './screens/EditContactScreen'
+import ViewContactScreen from './screens/ViewContactScreen'
+
+const MainNavigator = createStackNavigator(
+  {
+    Home:{screen : HomeScreen},
+    Add:{screen:AddNewContactScreen},
+    View:{screen:ViewContactScreen},
+    Edit:{screen:EditContactScreen}
+  },{
+    defaultNavigationOptions:{
+      headerTintColor:'#fff',
+      headerStyle:{
+        backgroundColor:'#b83227'
+      },
+      headerTitleStyle:{
+        color:"#fff"
+      }
+    }
+  }
+);
+
+const App =createAppContainer(MainNavigator)
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,FlatList, AsyncStorage } from 'react-native';
+import { Card } from 'native-base';
+import { Entypo } from '@expo/vector-icons'
+
+export default class HomeScreen extends React.Component {
+  state={
+    data:[]
+  }
+  static navigationOptions = {
+    title:"Contact App"
+  }
+ componentDidMount(){
+   const { navigation } =this.props;
+   navigation.addListener("willFocus",()=>{
+     this.getAllContact();
+   })
+ }
+  // Todo: get all contacts
+
+  getAllContact =async() =>{
+    //collects all data
+    await AsyncStorage.getAllKeys()
+    .then(keys=>{
+      return AsyncStorage.multiGet(keys)
+      .then(
+        result=>{
+          this.setState({
+            data:result
+          })
+        }
+      )
+      .catch(error=>{
+        console.log(error)
+      })
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+
+    console.log(this.state.data)
+  }
+  render(){
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={this.state.data}
+        renderItem={({item})=>{
+          let contact=JSON.parse(item[1]);
+          return (
+               <TouchableOpacity 
+                 onPress={()=>{
+                   this.props.navigation.navigate("View",{
+                     key:item[0].toString()
+                   })
+                 }}
+               >
+                 <Card style={styles.listItem}>
+                   <View style={styles.iconContainer}>
+                      <Text style={styles.contactIcon}>{contact.fname[0].toUpperCase()}</Text>
+                   </View>
+                   <View style={styles.infoContainer}>
+                     <Text style={styles.infoText}>
+                       {contact.fname} {contact.lname}
+                     </Text>
+                     <Text style={styles.infoText}>
+                       {contact.phone}
+                     </Text>
+                   </View>
+                 </Card>
+               </TouchableOpacity>
+          )
+        }}
+        keyExtractor={(item,index)=>item[0].toString()}
+      />
+      <TouchableOpacity
+      style={styles.floatButton}
+         onPress={()=>{
+           this.props.navigation.navigate("Add")
+         }}
+      >
+        <Entypo
+        name="plus"
+        size={30}
+        color="#fff"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  listItem: {
+    flexDirection: "row",
+    padding: 20
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  },
+  contactIcon: {
+    fontSize: 28,
+    color: "#fff"
+  },
+  infoContainer: {
+    flexDirection: "column"
+  },
+  infoText: {
+    fontSize: 16,
+    fontWeight: "400",
+    paddingLeft: 10,
+    paddingTop: 2
+  },
+  floatButton: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    position: "absolute",
+    bottom: 15,
+    right: 15 ,
+    height: 60,
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  }
+});
